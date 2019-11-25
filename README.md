562f4773f3c4cd06b80ab700ec58747f63d91152
cc-spranger <michael.spranger@codecentric.de>
2019-11-25 22:14:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rxjs-ojzdi9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-ojzdi9)
---
+++ test.ts
@@ -0,0 +1,9 @@
+import { of } from 'rxjs'; 
+import { map } from 'rxjs/operators';
+
+it('should be ok', ()=> {
+
+const values = { a: 1, b: 2, c: 3, x: 2, y: 4, z: 6 };
+// const source = cold("-a-b-c-|");
+const expected = "-x-y-z-|";
+});
